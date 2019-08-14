644eb666fa8d637c95261bb055957ae7c7f24aca
SirineKefi <sirine.kefi@esprit.tn>
2019-08-14 09:07:54 +0200

    Template_add1

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -95,7 +95,7 @@
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <!--<h6 class="collapse-header"></h6>-->
-            <a class="collapse-item" href="{{action('')}}">Client</a>
+            <a class="collapse-item" href="">Client</a>
             <a class="collapse-item" href="">Template</a>
             <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
             <div class="collapse-divider"></div>
